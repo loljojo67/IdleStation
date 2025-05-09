6770b0d7f6945bf24ca8662f2773a7bfed8f9cd0
jojo <loljojo67@gmail.com>
2025-05-10 00:00:43 +0200

    truc

--- My project/Assets/ClickManager.cs
+++ My project/Assets/ClickManager.cs
@@ -7,11 +7,14 @@ using UnityEngine;
 public class ClickManager : MonoBehaviour
 {
     [SerializeField] TMP_Text argent_conteur;
-    public double prixProduction = 1;
+    [SerializeField] TMP_Text idleBonusTxt;
+    [SerializeField] TMP_Text cliqueBonusText;
+
+    public int prixProduction = 1;
     public double tauxDeProduction = 1;
-    public double prixClique = 1;
+    public int prixClique = 1;
     public double tauxDeClique = 1; 
-    public double argent = 0;
+    public int argent = 0;
     [SerializeField] int lieuDeProduction = 1;
 
     public float delay = 1;
@@ -22,7 +25,10 @@ public class ClickManager : MonoBehaviour
 
     void Update()
     {       
+
         argent_conteur.text = math.floor(argent).ToString();
+        idleBonusTxt.text = prixProduction.ToString();
+        cliqueBonusText.text = prixClique.ToString();
     }
 
     public void IdleBonus()
@@ -45,10 +51,10 @@ public class ClickManager : MonoBehaviour
 
     public void Idle()
     {
-        argent += tauxDeProduction;
+        argent += (int)math.floor(tauxDeProduction);
     }
 
     public void Ajout(){
-        argent += tauxDeClique;
+        argent += (int)math.floor(tauxDeClique);
     }
 }
